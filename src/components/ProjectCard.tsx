f1851b637452bd572cf66408243027e28cacb6ec
Ian <i.seto00@gmail.com>
2022-09-22 20:25:43 -0400

    minor commit

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,29 +2,29 @@ import { Paper, Card, CardContent, CardActions, Button } from '@material-ui/core
 
 const ProjectCard = (props: any) => {
   return (
-    <Paper elevation={12} style={{backgroundColor: "#212121"}}>
-      <Card className="project-card-container" style={{backgroundColor: "#212121"}}>
-        <CardContent style={{color: "white"}}>
-          <h1>
-            {props.title}
-          </h1>
-          <h2>
-            {props.date}
-          </h2>
-          <h3>
-            {props.techStack}
-          </h3>
-          <p>
-            {props.description}
-          </p>
-        </CardContent>
-        <CardActions className="project-card-actions">
-          <Button className="project-card-link-button" size="small" href={props.link} target="_blank" style={{color: "white"}}>
-            Link
-          </Button>
-        </CardActions>
-      </Card>
-    </Paper>
+    <div className="project-item-container">
+      <Paper className="project-paper-container" elevation={12} style={{backgroundColor: "#212121"}}>
+        <Card className="project-card-container" style={{backgroundColor: "#212121"}}>
+          <CardContent style={{color: "white"}}>
+            <img className="card-media" src={process.env.PUBLIC_URL + '/React-icon.svg.png'} alt="" />
+            <h1>
+              {props.title}
+            </h1>
+            <h2>
+              {props.date}
+            </h2>
+            <h3>
+              {props.techStack}
+            </h3>
+          </CardContent>
+          <CardActions className="project-card-actions">
+            <Button className="project-card-link-button" size="small" href={props.link} target="_blank" style={{color: "white"}}>
+              Link
+            </Button>
+          </CardActions>
+        </Card>
+      </Paper>
+    </div> 
   );
 }
 
