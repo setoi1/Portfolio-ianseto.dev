3712965d8dade0f5b406db037643d1c5fd374aa4
Ian Seto <i.seto00@gmail.com>
2023-11-27 03:44:38 -0500

    new changes to styling

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -12,7 +12,7 @@ export default function About() {
           <br></br>
           <p>I have professional experience developing investment portfolio and fund management cloud platforms within the financial technology industry and I'm always excited to jump into new industries to build different types of products, whether it be in full-stack, backend, or frontend development.</p>
           <br></br> 
-          <p>In my free time, i love gaming, traveling, exploring new music, watching basketball, and I'm always pushing myself to learn new technologies through my career and various personal projects around my hobbies as well.</p>
+          <p>In my free time, I love gaming, traveling, exploring new music, watching basketball, and I'm always pushing myself to learn new technologies through my career and various personal projects around my hobbies as well.</p>
         </div>
       </div>
     </section>
