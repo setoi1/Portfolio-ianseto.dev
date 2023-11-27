d1a789a07df4000bc106066d41029bac08cef28a
Ian Seto <i.seto00@gmail.com>
2023-11-27 03:44:38 -0500

    new changes to styling

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,18 +1,18 @@
-import { Typography } from '@mui/material';
+
 import './About.css';
 
 export default function About() {
   return (
     <section id='about' className='about'>
       <div className='about-wrapper'>
-        <Typography variant='h2'>About Me</Typography>
+        <h2>About Me</h2>
         <hr></hr>
         <div className='about-summary'>
-          <Typography variant='body2'>Graduated with a Bachelor of Science in Computer Science from Wentworth Institute of Technology, with a passion for developing innovative software that creates a real-world impact in any industry.</Typography> 
+          <p>graduated with a bachelor of science in computer science from wentworth institute of technology, with a passion for developing innovative software that creates a real-world impact in any industry.</p> 
           <br></br>
-          <Typography variant='body2'>Experienced in developing cloud platforms within the Financial Technology industry and always excited to jump into new industries to build different types of products, whether it be fullstack, backend, or frontend development.</Typography>
+          <p>experienced in developing cloud platforms within the financial technology industry and always excited to jump into new industries to build different types of products, whether it be fullstack, backend, or frontend development.</p>
           <br></br> 
-          <Typography variant='body2'>In my free time, I love gaming, traveling, exploring new music, watching basketball, and am always pushing myself to learn new technologies through my career and various personal projects around my hobbies as well.</Typography>
+          <p>in my free time, i love gaming, traveling, exploring new music, watching basketball, and am always pushing myself to learn new technologies through my career and various personal projects around my hobbies as well.</p>
         </div>
       </div>
     </section>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,125 +1,129 @@
-  import { motion } from 'framer-motion';
-  import { Typography } from '@mui/material';
-  import './Projects.css';
+import './Projects.css';
 
-  export default function Projects() {
-    const projects = [
+const projects = [
+  {
+    id: 0,
+    image: 'SwiftAPI.png',
+    name: 'SwiftAPI',
+    description: 'A platform for developers to monetize their APIs and for users to purchase APIs in just a few clicks. Provides analytics, monitoring, and loggin features.',
+    techStack: [
       {
         id: 0,
-        image: 'SwiftAPI.png',
-        name: 'SwiftAPI',
-        description: 'A platform for developers to monetize their APIs and for users to purchase APIs in just a few clicks. Provides analytics, monitoring, and loggin features.',
-        techStack: [
-          {
-            id: 0,
-            name: 'React'
-          },
-          {
-            id: 1,
-            name: 'TypeScript'
-          },
-          {
-            id: 2,
-            name: 'Node.js'
-          },
-          {
-            id: 3,
-            name: 'MongoDB'
-          }
-        ],
-        link: 'https://github.com/setoi1/SwiftAPI'
+        name: 'React'
       },
       {
         id: 1,
-        image: 'FiMan.png',
-        name: 'Finance Manager',
-        description: 'A finance manager application built with Python.',
-        techStack: [
-          {
-            id: 0,
-            name: 'Python'
-          }, 
-          {
-            id: 1, 
-            name: 'tkinter',
-          }, 
-          {
-            id: 2,
-            name: 'RESTful APIs'
-          }
-        ],
-        link: 'https://github.com/marinom1/FiMan-Financial_Manager'
+        name: 'TypeScript'
       },
       {
         id: 2,
-        image: 'Stock.png',
-        name: 'Million Ether Page',
-        description: '',
-        techStack: [
-          {
-            id: 0,
-            name: 'Solidity',
-          },
-          {
-            id: 1,
-            name: 'Etherium',
-          },
-          {
-            id: 2,
-            name: 'HTML',
-          },
-          {
-            id: 3,
-            name: 'CSS',
-          },
-        ],
-        link: 'https://github.com/faceyacc/million_eth_homepage'
+        name: 'Node.js'
       },
       {
         id: 3,
-        image: 'Stock.png',
-        name: 'Stock Market Bot',
-        description: 'A bot to display user-requested stock information such as news, prices, and company information.',
-        techStack: [
-          {
-            id: 0,
-            name: 'JavaScript'
-          },
-          {
-            id: 1,
-            name: 'Node.js'
-          },
-          {
-            id: 2,
-            name: 'RESTful APIs'
-          }
-        ],
-        link: 'https://github.com/setoi1/Stock-Market-Discord-Bot'
+        name: 'MongoDB'
+      }
+    ],
+    link: 'https://github.com/setoi1/SwiftAPI'
+  },
+  {
+    id: 1,
+    image: 'FiMan.png',
+    name: 'Finance Manager',
+    description: 'A finance manager application built with Python.',
+    techStack: [
+      {
+        id: 0,
+        name: 'Python'
+      }, 
+      {
+        id: 1, 
+        name: 'tkinter',
+      }, 
+      {
+        id: 2,
+        name: 'RESTful APIs'
+      }
+    ],
+    link: 'https://github.com/marinom1/FiMan-Financial_Manager'
+  },
+  {
+    id: 2,
+    image: 'Stock.png',
+    name: 'Million Ether Page',
+    description: '',
+    techStack: [
+      {
+        id: 0,
+        name: 'Solidity',
       },
-    ];
+      {
+        id: 1,
+        name: 'Etherium',
+      },
+      {
+        id: 2,
+        name: 'HTML',
+      },
+      {
+        id: 3,
+        name: 'CSS',
+      },
+    ],
+    link: 'https://github.com/faceyacc/million_eth_homepage'
+  },
+  {
+    id: 3,
+    image: 'Stock.png',
+    name: 'Stock Market Bot',
+    description: 'A bot to display user-requested stock information such as news, prices, and company information.',
+    techStack: [
+      {
+        id: 0,
+        name: 'JavaScript'
+      },
+      {
+        id: 1,
+        name: 'Node.js'
+      },
+      {
+        id: 2,
+        name: 'RESTful APIs'
+      }
+    ],
+    link: 'https://github.com/setoi1/Stock-Market-Discord-Bot'
+  },
+];
 
-    return (
-      <section id='projects' className='projects'>
-        <div className='projects-wrapper'>
-          <Typography variant='h2'>Projects</Typography>
-          <div className='projects-content'>
-            <ul className='projects-list'>
-              {projects.map(project => 
-                <>
-                  <hr></hr>
-                  <a href={project.link}>
-                    <li key={project.id} className='project-item'>
-                      <div className='text-here'>
-                        {project.name}
-                      </div>
-                    </li>
-                  </a>
-                </>
-              )}
-            </ul>
-          </div>
-          <a href='https://github.com/setoi1/'>View All Projects</a>
+export default function Projects() {
+  return (
+    <section id='projects' className='projects'>
+      <div className='projects-wrapper'>
+        <h2>Projects</h2>
+        <div className='projects-content'>
+          <ul className='projects-list'>
+            {projects.map(project => 
+              <>
+                <hr></hr>
+                <a href={project.link}>
+                  <li key={project.id} className='project-item'>
+                    <div className='text-here'>
+                      {project.name}
+                    </div>
+                  </li>
+                </a>
+              </>
+            )}
+          </ul>
         </div>
-      </section>
-    );
-  }
+        <a 
+          href='https://github.com/setoi1/' 
+          rel='noreferrer' 
+          target='_blank' 
+        >
+          View All Projects
+        </a>
+      </div>
+    </section>
+  );
+}
