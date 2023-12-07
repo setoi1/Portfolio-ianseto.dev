e8a94e2cb6bcc294df3830511a1f5b191567120e
setoi1 <i.seto00@gmail.com>
2023-12-07 13:25:08 -0500

    typo fixes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,11 +8,11 @@ export default function About() {
         <h2>About Me</h2>
         <hr></hr>
         <div className='about-summary'>
-          <p>Graduated with a Bacherlor of Science in Computer Science from Wentworth Institute of Technology,  I specialize in full-stack software development with a passion for creating innovative software that enables real and positive impact in society.</p> 
+          <p>Graduated with a Bachelor of Science in Computer Science from Wentworth Institute of Technology, I specialize in full-stack software development with a passion for creating innovative software that enables real and positive impact in society.</p> 
           <br></br>
-          <p>I have professional experience developing investment portfolio and fundmanagement cloud platforms within the financial technology industry and always excited to jump into new industries to build different types of products, whether it be full-stack, backend, or frontend development.</p>
+          <p>I have professional experience developing investment portfolio and fund management cloud platforms within the financial technology industry and I'm always excited to jump into new industries to build different types of products, whether it be in full-stack, backend, or frontend development.</p>
           <br></br> 
-          <p>In my free time, i love gaming, traveling, exploring new music, watching basketball, and am always pushing myself to learn new technologies through my career and various personal projects around my hobbies as well.</p>
+          <p>In my free time, i love gaming, traveling, exploring new music, watching basketball, and I'm always pushing myself to learn new technologies through my career and various personal projects around my hobbies as well.</p>
         </div>
       </div>
     </section>
